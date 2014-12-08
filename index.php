8a4009520ad3d497cbcf7d4b7ae7af4447f08966
Andrew Angell <service@angelleye.com>
2014-12-08 06:23:08 -0600

    Adds index.php and assets

---
+++ index.php
@@ -0,0 +1,115 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PayPal PHP Class Library Demo | PayPal Partner | Angell EYE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+    <!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+    <!--script src="/assets/js/less-1.3.3.min.js"></script-->
+    <!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+
+    <link href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="/assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/assets/js/scripts.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-12 column">
+            <div class="row clearfix">
+                <div class="col-md-6 column">
+                    <div id="angelleye_logo">
+                        <img alt="Angell EYE PayPal PHP Class Library Demo" src="/assets/images/logo.png">
+                    </div>
+                </div>
+                <div class="col-md-6 column">
+                    <div id="paypal_partner_logo">
+                        <img alt="PayPal Partner and Certified Developer" src="/assets/images/paypal-partner-logo.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="jumbotron well">
+                <h1>
+                    PayPal PHP Class Library Demo
+                </h1>
+                <p>
+                    This is a collection of small demo apps for a variety of PayPal checkout flows using our class library.
+                </p>
+                <p>
+                    <a class="btn btn-primary btn-large" href="https://github.com/angelleye/paypal-php-library">View Library on GitHub</a>
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="PayPal Express Checkout Basic Integration" src="/assets/images/paypal-express-checkout.jpg">
+                        <div class="caption">
+                            <h3>
+                                Express Checkout Basic
+                            </h3>
+                            <p>
+                                Take a look at a basic Express Checkout experience and then study the code to see how it's done!
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="300x200" src="http://lorempixel.com/600/200/city">
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="300x200" src="http://lorempixel.com/600/200/sports">
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
