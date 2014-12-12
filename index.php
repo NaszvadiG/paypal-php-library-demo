b69d22ef661fd332a3a6b577cb2c09a61b9d40ba
Andrew Angell <service@angelleye.com>
2014-12-11 23:30:02 -0600

    Completes basic Express Checkout demo and resolves #1

--- index.php
+++ index.php
@@ -1,124 +1,86 @@
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>PayPal PHP Class Library Demo | PayPal Partner | Angell EYE</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<meta charset="utf-8">
+<title>PayPal PHP Class Library Demo | PayPal Partner | Angell EYE</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
-    <!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
-    <!--script src="/assets/js/less-1.3.3.min.js"></script-->
-    <!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+<!--script src="/assets/js/less-1.3.3.min.js"></script-->
+<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
 
-    <link href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/css/style.css" rel="stylesheet">
+<link href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="/assets/css/style.css" rel="stylesheet">
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
     <script src="/assets/js/html5shiv.js"></script>
     <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="/assets/images/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="/assets/images/favicon.png">
-
-    <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/assets/js/scripts.js"></script>
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="/assets/images/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="/assets/images/favicon.png">
+<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/assets/js/scripts.js"></script>
 </head>
 
 <body>
 <div class="container">
-    <div class="row clearfix">
-        <div class="col-md-12 column">
-            <div class="row clearfix">
-                <div class="col-md-6 column">
-                    <div id="angelleye_logo">
-                        <img alt="Angell EYE PayPal PHP Class Library Demo" src="/assets/images/logo.png">
-                    </div>
-                </div>
-                <div class="col-md-6 column">
-                    <div id="paypal_partner_logo">
-                        <img alt="PayPal Partner and Certified Developer" src="/assets/images/paypal-partner-logo.png" />
-                    </div>
-                </div>
-            </div>
-            <div class="jumbotron well">
-                <h2>
-                    PayPal PHP Class Library Demo / Tutorials
-                </h2>
-                <p>
-                    This is a collection of small demo apps for a variety of PayPal checkout flows using our class library.
-                </p>
-                <p>
-                    <a class="btn btn-primary btn-large" href="https://github.com/angelleye/paypal-php-library">View Library on GitHub</a>
-                </p>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img alt="PayPal Express Checkout Basic Integration" src="/assets/images/paypal-express-checkout.jpg">
-                        <div class="caption">
-                            <h3>
-                                Express Checkout
-                            </h3>
-                            <h4>
-                                Basic
-                            </h4>
-                            <p>
-                                Take a look at a basic Express Checkout experience and then study the code to see how it's done!
-                            </p>
-                            <p>
-                                <a class="btn btn-primary" href="#">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img alt="PayPal Express Checkout Line Items Integration" src="/assets/images/paypal-express-checkout-with-line-items.jpg">
-                        <div class="caption">
-                            <h3>
-                                Express Checkout
-                            </h3>
-                            <h4>
-                                w/ Line Items
-                            </h4>
-                            <p>
-                                This expands on our basic Express Checkout demo to add individual order items to the API requests so that the data is available within PayPal's transaction details.
-                            </p>
-                            <p>
-                                <a class="btn btn-primary" href="#">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img alt="PayPal Express Checkout Digital Goods Integration" src="/assets/images/paypal-express-checkout-digital-goods.jpg">
-                        <div class="caption">
-                            <h3>
-                                Express Checkout
-                            </h3>
-                            <h4>
-                                Digital Goods
-                            </h4>
-                            <p>
-                                Learn how to implement digital goods (micro-processing rates) into PayPal Express Checkout.  This includes the option for embedded payments.
-                            </p>
-                            <p>
-                                <a class="btn btn-primary" href="#">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="row clearfix">
+    <div class="col-md-12 column">
+      <div class="row clearfix">
+        <div class="col-md-6 column">
+          <div id="angelleye_logo"> <a href="/"><img alt="Angell EYE PayPal PHP Class Library Demo" src="/assets/images/logo.png"></a> </div>
         </div>
+        <div class="col-md-6 column">
+          <div id="paypal_partner_logo"> <img alt="PayPal Partner and Certified Developer" src="/assets/images/paypal-partner-logo.png" /> </div>
+        </div>
+      </div>
+      <div class="jumbotron well">
+        <h2> PayPal PHP Class Library Demo / Tutorials </h2>
+        <p> This is a collection of small demo apps for a variety of PayPal checkout flows using our class library. </p>
+        <p> <a class="btn btn-primary btn-large" href="https://github.com/angelleye/paypal-php-library">View Library on GitHub</a> </p>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="thumbnail"> <img alt="PayPal Express Checkout Basic Integration" src="/assets/images/paypal-express-checkout.jpg">
+            <div class="caption">
+              <h3> Express Checkout </h3>
+              <h4> Basic </h4>
+              <p> Take a look at a basic Express Checkout experience and then study the code to see how it's done! </p>
+              <p> <a class="btn btn-primary" href="demo/express-checkout-basic/index.php">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a> </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail"> <img alt="PayPal Express Checkout Line Items Integration" src="/assets/images/paypal-express-checkout-with-line-items.jpg">
+            <div class="caption">
+              <h3> Express Checkout </h3>
+              <h4> w/ Line Items </h4>
+              <p> This expands on our basic Express Checkout demo to add individual order items to the API requests so that the data is available within PayPal's transaction details. </p>
+              <p> <a class="btn btn-primary" href="#">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a> </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail"> <img alt="PayPal Express Checkout Digital Goods Integration" src="/assets/images/paypal-express-checkout-digital-goods.jpg">
+            <div class="caption">
+              <h3> Express Checkout </h3>
+              <h4> Digital Goods </h4>
+              <p> Learn how to implement digital goods (micro-processing rates) into PayPal Express Checkout.  This includes the option for embedded payments. </p>
+              <p> <a class="btn btn-primary" href="#">Demo</a> <a class="btn btn-info" href="#">Download Sample Code</a> </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 </body>
 </html>
